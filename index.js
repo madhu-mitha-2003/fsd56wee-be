1640b506c6ca756f71b0493808327753cde8ce69
madhumitha <you@example.com>
2024-08-28 21:03:26 +0530

    starting the express app

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-
-
-
 const mongoose = require('mongoose');
+const { MONGODB_URI} = require('./utils/config');
+const app = require('./app');
 
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB....');
 
-mongoose.connect()
-    .then(() => console.log('Connected to MongoDB......'))
+        app.listen(3001, () => {
+            console.log('Server is running on port 3001 a http://localhost:3001');
+        });
+    })
     .catch(err => console.error('Could not connect to MongoDB...', err));
  
